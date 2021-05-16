d5de8b943f28ac7093298e6e65c7c873bec0b4ee
MatthewNgan <58377002+MatthewNgan@users.noreply.github.com>
2021-05-16 20:24:24 +0800

    Update 2.2.6
    Fixed bug

--- src/views/StationView.js
+++ src/views/StationView.js
@@ -403,7 +403,7 @@ class StationInfoList extends React.Component {
               ...prevState?.stationRTData,
               [code]: {
                 ...prevState?.stationRTData[code],
-                [route.routeName]: tempArr
+                [`${route.routeName}-${route.direction}`]: tempArr
               },
             }
           }));
@@ -477,12 +477,12 @@ class StationInfoList extends React.Component {
                             color: 'white',
                             transform: 'scale(0.9)',
                           }
-                        }>{this.state?.stationRTData?.[sta[0]]?.[route.routeName]?.[0]?.stopsRemaining != null ?
+                        }>{this.state?.stationRTData?.[sta[0]]?.[`${route.routeName}-${route.direction}`]?.[0]?.stopsRemaining != null ?
                           <span style={{fontWeight: 'bold', color: 'white', fontSize: '1.25rem', fontFamily: 'Montserrat, sans-serif'}}>
                             <svg style={{color: 'white'}} xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-broadcast" viewBox="0 0 16 16">
                               <path style={{color: 'white'}} d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707zm2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708zm5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708zm2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
                             </svg>
-                            &nbsp;{this.state?.stationRTData?.[sta[0]]?.[route.routeName]?.[0]?.stopsRemaining > 0 ? `${this.state?.stationRTData?.[sta[0]]?.[route.routeName]?.[0]?.stopsRemaining} 站` : '到達'}
+                            &nbsp;{this.state?.stationRTData?.[sta[0]]?.[`${route.routeName}-${route.direction}`]?.[0]?.stopsRemaining > 0 ? `${this.state?.stationRTData?.[sta[0]]?.[`${route.routeName}-${route.direction}`]?.[0]?.stopsRemaining} 站` : '到達'}
                           </span>
                           : '未發車'}</button>
                       </li>
